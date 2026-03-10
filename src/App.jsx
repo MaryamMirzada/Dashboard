63a4f8324d7e42f0b83c4dbf4cba211385109180
Maryam Mirzada <marimmirzada@gmail.com>
2026-03-10 10:53:39 +0430

    intall MUI Package

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import {Typography} from '@mui/material'
+
+function App() {
+  return (
+    <div>
+    <Typography variant='h3'>
+      Dashboard
+    </Typography>
+    </div>
+  );
+}
+
+export default App
